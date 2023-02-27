057a9ac32d5a5c020defd4865dae2f2dd91dbe46
DaDanielL <daniellin@Daniels-MacBook-Pro-4.local>
2023-02-26 19:23:03 -0800

    added Nutrition class and tests

---
+++ include/Nutrition.h
@@ -0,0 +1,28 @@
+#ifndef NUTRITION_H
+#define NUTITION_H
+
+class Nutrition{
+    private:
+        // values all in grams
+        int carb;
+        int protein;
+        int fat;
+        int cal;
+
+    public:
+        Nutrition() : cal(0), carb(0), protein(0), fat(0){}
+        Nutrition(int cal, int carb, int protein, int fat) : cal(cal), carb(carb), protein(protein), fat(fat){}
+
+        int getCarb() const {return carb;}
+        int getProtein() const {return protein;}
+        int getFat() const {return fat;}
+        int getCal() const {return cal;}
+
+        void setCarb(int n){carb = n;}
+        void setProtein(int n){protein = n;}
+        void setFat(int n){fat = n;}
+        void setCal(int n){cal = n;}
+
+};
+
+#endif
--- test/nutritionTest.cpp
+++ test/nutritionTest.cpp
@@ -1,3 +1,34 @@
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
 #include "../include/Nutrition.h"
+
+TEST(NutritionTests, constructorWorks){
+    EXPECT_NO_THROW(new Nutrition(3,52,12,3));
+}
+TEST(NutritionTests, defaultConstructorWorks){
+    EXPECT_NO_THROW(new Nutrition());
+}
+TEST(NutritionTests, constructorsAssigningProperValues){
+    Nutrition n1(200, 20, 8, 10);
+    EXPECT_EQ(n1.getCal(), 200);
+    EXPECT_EQ(n1.getCarb(), 20);
+    EXPECT_EQ(n1.getProtein(), 8);
+    EXPECT_EQ(n1.getFat(), 10);
+
+    Nutrition n2;
+    EXPECT_EQ(n2.getCal(), 0);
+    EXPECT_EQ(n2.getCarb(), 0);
+    EXPECT_EQ(n2.getProtein(), 0);
+    EXPECT_EQ(n2.getFat(), 0);
+}
+TEST(NutritionTests, settersWorking){
+    Nutrition n;
+    n.setCal(80);
+    n.setCarb(20);
+    n.setProtein(5);
+    n.setFat(2);
+    EXPECT_EQ(n.getCal(), 80);
+    EXPECT_EQ(n.getCarb(), 20);
+    EXPECT_EQ(n.getProtein(), 5);
+    EXPECT_EQ(n.getFat(), 2);
+}
