0ec09f059fbf3a10c8a44dfd98a969671bfb9f47
DaDanielL <danieldaolin@gmail.com>
2023-02-26 23:50:19 -0800

    added Meal.h and beginning methods

---
+++ include/Meal.h
@@ -0,0 +1,26 @@
+#ifndef MEAL_H
+#define MEAL_H
+
+#include "Nutrition.h"
+#include "Food.h"
+#include <vector>
+#include <string>
+
+class Meal{
+    private: 
+        std::vector<Food> allFood;
+
+    public:
+        Meal();
+        Meal(std::vector<Food> allFood): allFood(allFood){}
+
+        std::vector<Food> getAllFood() const{return allFood;}
+        void addFood(Food f);
+        void removeFood(int i);
+        void removeFood(std::string name);
+
+        Nutrition calcTotalNutrition() const;
+
+};
+
+#endif
