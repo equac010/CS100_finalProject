a2d4839c8b2db26728ef36724740c76b6125f562
DaDanielL <danieldaolin@gmail.com>
2023-02-27 23:23:05 -0800

    added beginning methods to DailyInfo

---
+++ include/DailyInfo.h
@@ -0,0 +1,25 @@
+#ifndef DAILYINFO_H
+#define DAILYINFO_H
+
+#include "Nutrition.h"
+#include "Food.h"
+#include "Meal.h"
+
+class DailyInfo{
+    private:
+        Meal breakfast;
+        Meal lunch;
+        Meal dinner;
+        Meal snack;
+        Nutrition dailyTarget;
+
+    public:
+        Nutrition calcTotalNutrition() const;
+        bool dailyTargetReached() const;
+        void addFood(Meal m, Food f){m.addFood(f);}
+        void removeFood(Meal m, int n){m.removeFood(n);}
+        
+};
+
+
+#endif
